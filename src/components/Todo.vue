b221997e2f6c607891a08128b8632b1ac3975d0c
Supakrit <supakrit@Supakrits-MacBook-Air.local>
2021-02-10 12:18:45 +0700

    Create Todo Page

---
+++ src/components/Todo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <section class="todoapp">
+      <header class="header">
+        <h1>{{ title }}</h1>
+        <input
+            class="new-todo"
+            placeholder="What needs to be done?"
+            autofocus
+        />
+      </header>
+
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "todo",
+  data() {
+    return {
+      title: 'Todo-List',
+    };
+  },
+};
+</script>
